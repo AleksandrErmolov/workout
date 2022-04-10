920d43155b02932edd3156eb632f238d80cfa1b1
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-04-10 16:28:13 +0300

    add final

--- back/config/db.js
+++ back/config/db.js
@@ -1,18 +1,17 @@
 import mongoose from 'mongoose'
-import { MONGO_URL } from '../../test'
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(MONGO_URL, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        })
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+		})
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-    } catch (error) {
-        console.error(`Error: ${error.message}`.red.underline.bold)
-        process.exit(1)
-    }
+		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+	} catch (error) {
+		console.error(`Error: ${error.message}`.red.underline.bold)
+		process.exit(1)
+	}
 }
--- back/server.js
+++ back/server.js
@@ -4,17 +4,20 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import path from 'path'
 
-//Config
+/* Config */
 import { connectDB } from './config/db.js'
 
-// Routes
-import userRoutes from './routes/userRouters.js'
+/* Middleware */
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
+/* Routes */
+import userRoutes from './routes/userRoutes.js'
+import exerciseRoutes from './routes/exerciseRoutes.js'
+import workoutRoutes from './routes/workoutRoutes.js'
 
 dotenv.config()
 
 connectDB()
-
 
 const app = express()
 
@@ -22,7 +25,25 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 app.use(express.json())
 
+const __dirname = path.resolve()
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads/')))
+
 app.use('/api/users', userRoutes)
+app.use('/api/exercises', exerciseRoutes)
+app.use('/api/workouts', workoutRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+	// Step 1:
+	app.use(express.static(path.resolve(__dirname, './client/build')))
+	// Step 2:
+	app.get('*', function (request, response) {
+		response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+	})
+}
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
